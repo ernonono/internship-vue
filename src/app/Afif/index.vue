540b1ef5ebf8a138fc46f94fa8573a3a44d049dc
Afif Nugroho <92508998+Apips1@users.noreply.github.com>
2023-09-07 13:23:20 +0700

    login vue js afif

---
+++ src/app/Afif/index.vue
@@ -0,0 +1,86 @@
+<route lang="yaml">
+meta:
+  layout: guest
+</route>
+<script lang="ts">
+  // export default {
+  //   name: 'index',
+  //   data() {
+  //     return {
+  //       login: 'LOGIN',
+  //       samping: 'Sekarang',
+  //     }
+  //   },
+  // }
+</script>
+<template>
+  <n-space justify="center">
+    <n-card>
+      <n-carousel autoplay>
+        <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+        />
+        <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+        />
+        <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+        />
+        <img
+          class="carousel-img"
+          src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+        />
+      </n-carousel>
+      <n-space justify="center">
+        <h1>LOGIN</h1>
+        <n-gradient-text
+          gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)"
+        >
+          <h1>Sekarang</h1>
+        </n-gradient-text>
+      </n-space>
+
+      <n-form>
+        <n-form-item path="email" label="Email">
+          <n-input round placeholder="Masukan Email Anda" />
+        </n-form-item>
+        <n-form-item path="password" label="Password">
+          <n-input
+            round
+            placeholder="Masukan Password"
+            type="password"
+            show-password-on="mousedown"
+          />
+        </n-form-item>
+        <n-space justify="center">
+          <router-link to="/test"
+            ><n-button type="primary">LOGIN</n-button></router-link
+          >
+        </n-space>
+      </n-form>
+    </n-card>
+  </n-space>
+</template>
+<style scoped>
+  .n-card {
+    background-color: theme('colors.white');
+    border-radius: theme('borderRadius.lg');
+    padding: theme('spacing.6');
+    box-shadow: theme('boxShadow.xl');
+    width: 600px;
+    margin-top: 50px;
+  }
+  .carousel-img {
+    width: 100%;
+    height: 240px;
+    object-fit: cover;
+  }
+  .n-button {
+    border-radius: 0.375rem;
+    width: 10vw;
+    height: 5vh;
+  }
+</style>
