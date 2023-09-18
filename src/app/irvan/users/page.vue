818f78c684a9430fb0fea76ce06645436c2985f2
Irvan Alfian Adi N <92213859+Irvana875Uns@users.noreply.github.com>
2023-09-18 16:17:57 +0700

    user

---
+++ src/app/irvan/users/page.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useHttp } from '@/composables/http/http'
+
+const { data } = useHttp('http://reqres.in/api/users')
+</script>
+<template>
+  <div v-for="user in data?.data" :key="user.id" @click="$router.push(`/irvan/users/${user.id}`)">
+    <n-card :title="user.first_name"></n-card>
+  </div>
+  <!-- <div>{{ data?.data }}</div> -->
+</template>
